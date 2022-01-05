66a17629b8bd0b6c462ce89dbdb8b044c3a2de5f
stanislaw7 <estanislau.gb@gmail.com>
2022-01-05 10:58:36 -0300

    Estilização final dos 'radio buttons'

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished"
+import { darken, transparentize } from "polished"
 
 export const Container = styled.form`
   h2{
@@ -61,6 +61,12 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -68,7 +74,9 @@ export const RadioBox = styled.button<RadioBoxProps>`
   border: 1px solid #D7D7D7;
   border-radius: 0.25rem;
 
-  background: ${({ isActive }) => isActive ? '#EEE' : 'transparent'};
+  background: ${({ isActive, activeColor }) => isActive
+    ? transparentize(0.85, colors[activeColor])
+    : 'transparent'};
 
   display: flex;
   align-items: center;
